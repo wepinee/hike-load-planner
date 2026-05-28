23b9977228d3d5bd2bb1cfb9622d0a5e624100fb
wepinee <159862861+wepinee@users.noreply.github.com>
2026-05-29 02:29:28 +0800

    Delete scripts directory

--- scripts/fix-assignments-nullable.sql
+++
@@ -1,5 +0,0 @@
--- Выполните в MySQL Workbench, если раскладка падает с "gear_item_id cannot be null"
-USE hike_load;
-
-ALTER TABLE assignments MODIFY COLUMN gear_item_id BIGINT NULL;
-ALTER TABLE assignments MODIFY COLUMN food_item_id BIGINT NULL;
